2a4fcdfbb4db00346420eb5122f7596fef1e4c6c
wilson lopez <wilson.lopez@tecsup.edu.pe>
2025-08-28 15:00:04 -0500

    Responsable: agregar StudentController inicial

---
+++ src/main/java/com/grp02/StudentController.java
@@ -0,0 +1,7 @@
+package com.grp02;
+
+public class StudentController {
+    public static void main(String[] args) {
+        System.out.println("StudentController creado por el responsable.");
+    }
+}
